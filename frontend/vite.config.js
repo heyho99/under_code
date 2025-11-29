d9264afb0c86511b5c5d686eeb878be78cafd460
heyho99 <178541829+heyho99@users.noreply.github.com>
2025-11-29 20:52:07 +0900

    loginをbffに委譲

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 
+// あくまでもコンテナ内部の設定（dockerがポートフォワーディングの設定を持っている）
 export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 80,
     watch: {
       usePolling: true,
+    },
+    // frontendからfetch('/api/v1/auth/login')を叩くと、http://localhost:8081/api/v1/auth/login に飛ぶ
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+      },
     },
   },
   define: {
