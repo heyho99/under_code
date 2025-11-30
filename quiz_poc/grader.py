1123686733ac74af6a954c587891afc7a5401e94
heyho99 <ouchi.job22@gmail.com>
2025-11-30 09:46:21 +0900

    pocを行う

---
+++ quiz_poc/grader.py
@@ -0,0 +1,10 @@
+def compare_output(expected_output, actual_output):
+    pass
+
+
+def grade_quiz(quiz, execution_result):
+    pass
+
+
+def format_result(is_correct, expected_output, actual_output):
+    pass
---
+++ quiz_poc/js/run_code.js
@@ -0,0 +1,10 @@
+function setupRunButton() {}
+
+function handleRunButtonClick() {}
+
+function sendCodeToExecutor(code) {}
+
+function receiveExecutionResult(result) {}
+
+document.addEventListener("DOMContentLoaded", function () {
+});
